f3d5c585d845efd30a4ca5a1f947caa653060be1
Spencer Pollock <srepollock@gmail.com>
2019-10-12 01:29:50 -0700

    feat: 🎸 Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,16 @@ module.exports = {
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: 'UA-21240152-6',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
   ],
 }
