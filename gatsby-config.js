803a169dea5ea1679a5626bfc999329e3c398386
Spencer Pollock <spencer@spollock.ca>
2022-01-08 12:50:29 -0800

    fix: icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'static/assets/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'images/favicon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-react-helmet',
