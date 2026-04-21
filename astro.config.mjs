229215bcc65c3517a9d9f3acd69510612e543b95
Spencer Pollock <spencer@spollock.ca>
2026-04-20 23:45:28 -0700

    Configure site URL and GitHub Pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+    site: "https://spollock.ca",
+    integrations: [mdx()],
 });
